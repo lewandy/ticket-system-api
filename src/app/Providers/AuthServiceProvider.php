464e7d6f6f59f807b8788dee975ae8ee0d071487
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2019-12-03 14:35:20 -0400

    expire tokens in one days

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -27,5 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(1));
     }
 }
