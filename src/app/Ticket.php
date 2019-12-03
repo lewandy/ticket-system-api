80543300e11fdfba2330a1ba936fc01b5cf7fd60
Lewandy Dilone Bonifacio <lewandydilone1@live.com>
2019-12-02 23:14:02 -0600

    add relationships to TicketModel

--- src/app/Ticket.php
+++ src/app/Ticket.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+
+    public function employee() {
+        return $this->belongsTo('App\Employee');
+    }
+
+    public function status() {
+        return $this->belongsTo('App\TicketStatus');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
